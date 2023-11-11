7d4decc20332bf5cb6d249a66c4d6628d2b5d7b1
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-11-11 09:43:37 +0400

    Initial commit

---
+++ firstTask/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace mathModeling
+{
+  internal abstract class Program
+  {
+    private static double F(double x)
+    {
+      var numerator = Math.Pow(x - 1, 2);
+      var denominator = x * x + 1;
+
+      return Math.Pow(numerator / denominator, 1.0 / 3.0);
+    }
+
+    public (double, double) SvennsMethod(double x)
+    {
+      var h = 0.5;
+      var solution = F(x);
+
+      Console.WriteLine("Решение уравнения: " + solution);
+
+      // Применение метода Свенна для поиска отрезка, содержащего точку минимума
+      var a = x - h; // Левая граница интервала
+      var b = x + h; // Правая граница интервала
+
+      var fa = F(a);
+      var fb = F(b);
+
+      if (fa > solution && solution < fb) return (a, b);
+      else
+      {
+        if (fa < solution && solution < fb)
+        {
+          h = -h;
+          (a, b) = (b, a);
+        }
+      }
+
+      while (fa < fb)
+      { 
+        x += h;
+        solution = F(x);
+        a = x - h;
+        b = x + h;
+        fa = F(a); fb = F(b);
+      }
+
+      if (h < 0) (a, b) = (b, a);
+
+      Console.WriteLine("Отрезок, содержащий точку минимума: [" + a + ", " + b + "]");
+
+      return (a, b);
+    }
+    
+    public static void Main(string[] args)
+    {
+    }
+  }
+}
---
+++ fourTask/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace fourTask
+{
+  internal abstract class Program
+  {
+    private static float F(float x, float y) => x + 3 * y;
+    
+    public static void Main(string[] args)
+    {
+      const int n = 4;
+      var a = new float[20, 2];
+      var b = new float[20];
+      float x, y, x1 = 0, x2 = 0; 
+      float max = -100;
+      
+      a[0, 0] = 1; a[0, 1] = 4; b[0] = 4;
+      a[1, 0] = 1; a[1, 1] = 1; b[1] = 6;
+      a[2, 0] = 1; a[2, 1] = 0; b[2] = 2;
+      a[3, 0] = 0; a[3, 1] = 1; b[3] = 0;
+
+      for (var i = 0; i < n; i++)
+      { 
+        for (var j = 0; j < n; j++)
+        {
+          if (Math.Abs(a[i, 0] * a[j, 1] - a[i, 1] * a[j, 0]) > 0.001)
+          {
+            x = (b[i] * a[j, 1] - a[i, 1] * b[j]) / (a[i, 0] * a[j, 1] - a[i, 1] * a[j, 0]);
+            y = (a[i, 0] * b[j] - b[i] * a[j, 0]) / (a[i, 0] * a[j, 1] - a[i, 1] * a[j, 0]);
+            var t = 1;
+            var result = false;
+            Console.WriteLine($"{x} = {y} {i} {j}");
+            for (var k = 0; k < n; k++)
+            {
+              switch (k)
+              {
+                case 1:
+                  if (a[k, 0] * x + a[k, 1] * y < b[k]) t = 0;
+                  break;
+                case 2:
+                  if (a[k, 0] * x + a[k, 1] * y > b[k]) t = 0;
+                  break;
+                case 3:
+                  if (a[k, 0] * x + a[k, 1] * y < b[k]) t = 0;
+                  break;
+                case 4:
+                  if (a[k, 0] * x + a[k, 1] * y < b[k]) t = 0;
+                  break;
+              }
+            }
+            Console.WriteLine(t);
+
+            if (t > 0) result = true;
+            if ((result) || ((F(x, y) > max)))
+            {
+              max = F(x, y);
+              x1 = x;
+              x2 = y;
+            }
+          }
+        }
+      }
+      Console.WriteLine(x1 + " " + x2 + " " + max);
+    }
+  }
+}
