15afd0155c0b9193c57bfedab73978046f44c394
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-11-26 20:43:29 +0400

    Updated syntax

--- firstTask/Program.cs
+++ firstTask/Program.cs
@@ -1,105 +1,103 @@
 ﻿using System;
 
-namespace mathModeling
+namespace mathModeling;
+internal abstract class Program
 { 
-  internal abstract class Program
-  { 
-    // function
-    private static double s_function(double x)
-    {
-      var numerator = Math.Pow(x - 1, 2);
-      var denominator = x * x + 1;
+  // function
+  private static double s_function(double x)
+  {
+    var numerator = Math.Pow(x - 1, 2);
+    var denominator = x * x + 1;
 
-      return Math.Pow(numerator, 1.0 / 3.0) / denominator;
-    }
+    return Math.Pow(numerator, 1.0 / 3.0) / denominator;
+  }
         
-    // Svenn method
-    private static (double, double) s_svennsMethod(double x)
-    {
-      var h = 0.5;
-      var solution = s_function(x);
+  // Svenn method
+  private static (double, double) s_svennsMethod(double x)
+  {
+    var h = 0.5;
+    var solution = s_function(x);
 
-      Console.WriteLine("Решение уравнения: " + solution); 
+    Console.WriteLine("Решение уравнения: " + solution); 
       
-      var a = x - h;
-      var b = x + h;
+    var a = x - h;
+    var b = x + h;
 
-      var fa = s_function(a);
-      var fb = s_function(b);
+    var fa = s_function(a);
+    var fb = s_function(b);
 
-      Console.WriteLine(fa + " " + solution + " " + fb);
-      if (fa < solution && solution > fb) {
-        Console.WriteLine("Отрезок, содержащий точку минимума: [" + a + ", " + b + "]");
-        return (a, b);
-      }
-      else
-      { 
-        if (fa > solution && solution > fb)
-        {
-          h = -h;
-          (a, b) = (b, a);
-        }
-      }
-
-      while (fa > fb)
-      {
-        x += h;
-        a = x - h;
-        b = x + h;
-        fa = s_function(a); fb = s_function(b);
-      }
-
-      if (h > 0) (a, b) = (b, a);
-
+    Console.WriteLine(fa + " " + solution + " " + fb);
+    if (fa < solution && solution > fb) {
       Console.WriteLine("Отрезок, содержащий точку минимума: [" + a + ", " + b + "]");
-
       return (a, b);
     }
-
-    // method of golden ratio  
-    private static double s_FindMinimum(double x)
-    {
-      var result = s_svennsMethod(x);
-      var a = result.Item1;
-      var b = result.Item2;
-
-      const double e = 0.001;
-      var phi = (1 + Math.Sqrt(5)) / 2; 
-
-      var x1 = a + 0.382 * (b - a);
-      var x2 = b - 0.382 * (b - a);
-
-      var f1 = s_function(x1);
-      var f2 = s_function(x2);
-
-      while (Math.Abs(b - a) > e)
+    else
+    { 
+      if (fa > solution && solution > fb)
       {
-        if (f1 > f2)
-        {
-          b = x2;
-          x2 = x1;
-          f2 = f1;
-          x1 = a + (1 - phi) * (b - a);
-          f1 = s_function(x1);
-        }
-        else
-        {
-          a = x1;
-          x1 = x2;
-          f1 = f2;
-          x2 = b - (1 - phi) * (b - a);
-          f2 = s_function(x2);
-        }
+        h = -h;
+        (a, b) = (b, a);
       }
-
-      return (a + b) / 2;
     }
 
-    /* Main mehtod */
-    public static void Main(string[] args)
+    while (fa > fb)
     {
-      const double x = -1;
-      Console.WriteLine(s_FindMinimum(x));
+      x += h;
+      a = x - h;
+      b = x + h;
+      fa = s_function(a); fb = s_function(b);
     }
+
+    if (h > 0) (a, b) = (b, a);
+
+    Console.WriteLine("Отрезок, содержащий точку минимума: [" + a + ", " + b + "]");
+
+    return (a, b);
+  }
+
+  // method of golden ratio  
+  private static double s_FindMinimum(double x)
+  {
+    var result = s_svennsMethod(x);
+    var a = result.Item1;
+    var b = result.Item2;
+
+    const double e = 0.001;
+    var phi = (1 + Math.Sqrt(5)) / 2; 
+
+    var x1 = a + 0.382 * (b - a);
+    var x2 = b - 0.382 * (b - a);
+
+    var f1 = s_function(x1);
+    var f2 = s_function(x2);
+
+    while (Math.Abs(b - a) > e)
+    {
+      if (f1 > f2)
+      {
+        b = x2;
+        x2 = x1;
+        f2 = f1;
+        x1 = a + (1 - phi) * (b - a);
+        f1 = s_function(x1);
+      }
+      else
+      {
+        a = x1;
+        x1 = x2;
+        f1 = f2;
+        x2 = b - (1 - phi) * (b - a);
+        f2 = s_function(x2);
+      }
+    }
+
+    return (a + b) / 2;
+  }
+
+  /* Main mehtod */
+  public static void Main(string[] args)
+  {
+    const double x = -1;
+    Console.WriteLine(s_FindMinimum(x));
   }
 }
--- fourTask/Program.cs
+++ fourTask/Program.cs
@@ -1,64 +1,61 @@
 ﻿using System;
 
-namespace fourTask
+namespace fourTask;
+internal abstract class Program
 {
-  internal abstract class Program
-  {
-    // function method
-    private static float s_function(float x1, float x2) => (2 * x1) + (3 * x2);
+  // function method
+  private static float s_function(float x1, float x2) => (2 * x1) + (3 * x2);
     
-    /* Main method */
-    public static void Main(string[] args)
-    {
-      const int n = 4;
-      var a = new float[20, 2];
-      var b = new float[20];
-      float x1 = 0, x2 = 0;
-      float max = -100;
+  /* Main method */
+  public static void Main(string[] args)
+  {
+    const int n = 4;
+    var a = new float[20, 2];
+    var b = new float[20];
+    float x1 = 0, x2 = 0;
+    float max = -100;
       
-      a[0, 0] = 8;  a[0, 1] = -5; b[0] = 11;
-      a[1, 0] = -1; a[1, 1] = 3; b[1] = 1;
-      a[2, 0] = 2;  a[2, 1] = 7; b[2] = 7;
+    a[0, 0] = 8;  a[0, 1] = -5; b[0] = 11;
+    a[1, 0] = -1; a[1, 1] = 3; b[1] = 1;
+    a[2, 0] = 2;  a[2, 1] = 7; b[2] = 7;
 
-      for (var i = 0; i < n; i++)
-      { 
-        for (var j = 0; j < n; j++)
+    for (var i = 0; i < n; i++)
+    { 
+      for (var j = 0; j < n; j++)
+      {
+        if (!(Math.Abs(a[i, 0] * a[j, 1] - a[i, 1] * a[j, 0]) > 0.001)) continue;
+        var x = (b[i] * a[j, 1] - a[i, 1] * b[j]) / (a[i, 0] * a[j, 1] - a[i, 1] * a[j, 0]);
+        var y = (a[i, 0] * b[j] - b[i] * a[j, 0]) / (a[i, 0] * a[j, 1] - a[i, 1] * a[j, 0]);
+        var t = 1;
+        var result = false;
+          
+        Console.WriteLine($"{x} = {y} {i} {j}");
+          
+        for (var k = 0; k < n; k++)
         {
-          if (!(Math.Abs(a[i, 0] * a[j, 1] - a[i, 1] * a[j, 0]) > 0.001)) continue;
-          var x = (b[i] * a[j, 1] - a[i, 1] * b[j]) / (a[i, 0] * a[j, 1] - a[i, 1] * a[j, 0]);
-          var y = (a[i, 0] * b[j] - b[i] * a[j, 0]) / (a[i, 0] * a[j, 1] - a[i, 1] * a[j, 0]);
-          var t = 1;
-          var result = false;
-          
-          Console.WriteLine($"{x} = {y} {i} {j}");
-          
-          for (var k = 0; k < n; k++)
+          switch (k)
           {
-            switch (k)
-            {
-              case 1:
-                if (a[k, 0] * x + a[k, 1] * y <= b[k]) t = 0;
-                break;
-              case 2:
-                if (a[k, 0] * x + a[k, 1] * y <= b[k]) t = 0;
-                break;
-              case 3:
-                if (a[k, 0] * x + a[k, 1] * y >= b[k]) t = 0;
-                break;
-            }
+            case 1:
+              if (a[k, 0] * x + a[k, 1] * y <= b[k]) t = 0;
+              break;
+            case 2:
+              if (a[k, 0] * x + a[k, 1] * y <= b[k]) t = 0;
+              break;
+            case 3:
+              if (a[k, 0] * x + a[k, 1] * y >= b[k]) t = 0;
+              break;
           }
-          
-          Console.WriteLine(t);
-
-          if (t > 0) result = true;
-          if ((!result) && ((!(s_function(x, y) > max)))) continue;
-          max = s_function(x, y);
-          x1 = x;
-          x2 = y;
         }
+          
+        Console.WriteLine(t);
+
+        if (t > 0) result = true;
+        if ((!result) && ((!(s_function(x, y) > max)))) continue;
+        max = s_function(x, y);
+        x1 = x;
+        x2 = y;
       }
-      
-      Console.WriteLine(x1 + " " + x2 + " " + max);
     }
+    Console.WriteLine(x1 + " " + x2 + " " + max);
   }
 }
