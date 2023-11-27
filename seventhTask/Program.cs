c446010e7d8a727ae99b9bb67121a0ccc46fa33a
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-11-27 16:43:44 +0400

    Created the seventh solution

--- seventhTask/Program.cs
+++ seventhTask/Program.cs
@@ -6,39 +6,108 @@ namespace seventhTask
   {
     private static int V = 9;
 
-    /* Methods */
-
-
     /* Support methods */
+    // find the max distance
     private static int s_maxDistance(int[] dist, bool[] sptSet)
     {
-      // Initialize min value
-      int min = int.MaxValue, min_index = -1;
+      // Initialize max value
+      int max = int.MinValue, max_index = -1;
 
-       for (int v = 0; v < V; v++)
-                if (sptSet[v] == false && dist[v] <= min)
-                {
-                    min = dist[v];
-                    min_index = v;
-                }
-
-            return min_index;
+      for (int v = 0; v < V; v++)
+      {
+        if (sptSet[v] == false && dist[v] >= max)
+        {
+          max = dist[v];
+          max_index = v;
         }
+      }
 
-        /* Main method */
-        static void Main()
+      return max_index;
+    }
+
+    // print solution
+    private static void s_printSolution(int[] dist, int n)
+    {
+      Console.Write("Vertex Distance from Source\n");
+      for (int i = 0; i < V; i++)
+        Console.Write(i + " \t\t " + dist[i] + "\n");
+    }
+
+    /* Methods */
+    // the method of dijkstra
+    private static void dijkstra(int[,] graph, int src)
+    {
+      int[] dist = new int[V]; // The output array. dist[i]
+      // will hold the maximum
+      // distance from src to i
+
+      // sptSet[i] will true if vertex
+      // i is included in longest path
+      // tree or longest distance from
+      // src to i is finalized
+      bool[] sptSet = new bool[V];
+
+      // Initialize all distances as
+      // MIN_VALUE and stpSet[] as false
+      for (int i = 0; i < V; i++)
+      {
+        dist[i] = int.MinValue;
+        sptSet[i] = false;
+      }
+
+      // Distance of source vertex
+      // from itself is always 0
+      dist[src] = 0;
+
+      // Find longest path for all vertices
+      for (int count = 0; count < V - 1; count++)
+      {
+        // Pick the maximum distance vertex
+        // from the set of vertices not yet
+        // processed. u is always equal to
+        // src in first iteration.
+        int u = s_maxDistance(dist, sptSet);
+
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+
+        // Update dist value of the adjacent
+        // vertices of the picked vertex.
+        for (int v = 0; v < V; v++)
+        {
+          // Update dist[v] only if it is not in
+          // sptSet, there is an edge from u
+          // to v, and total weight of path
+          // from src to v through u is greater
+          // than current value of dist[v]
+          if (!sptSet[v] && graph[u, v] != 0 &&
+            dist[u] != int.MinValue && dist[u] + graph[u, v] > dist[v])
+            dist[v] = dist[u] + graph[u, v];
+        }
+      }
+
+      // print the constructed distance array
+      s_printSolution(dist, V);
+    }
+
+    /* Main method */
+    static void Main()
     {
       /* Let us create the example 
       graph discussed above */
-      var graph = new int[,] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-                               { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-                               { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-                               { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-                               { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                               { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-                               { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-                               { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-                               { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+      var graph = new int[,] { { 0, 4, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                               { 4, 0, 0, 6, 10, 8, 0, 0, 0, 0, 0, 0, 0 },
+                               { 6, 0, 0, 0, 9, 11, 6, 0, 0, 0, 0, 0, 0 },
+                               { 0, 6, 0, 0, 0, 0, 0, 9, 10, 0, 0, 0, 0 },
+                               { 0, 10, 9, 0, 0, 0, 0, 11, 16, 0, 0, 0, 0 },
+                               { 0, 8, 11, 0, 0, 0, 0, 8, 18, 2, 0, 0, 0 },
+                               { 0, 0, 6, 0, 0, 0, 0, 8, 8, 0, 0, 0, 0 },
+                               { 0, 0, 0, 9, 11, 0, 0, 0, 0, 0, 7, 8, 0 },
+                               { 0, 0, 0, 10, 16, 8, 0, 0, 0, 0, 4, 5, 0 },
+                               { 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 7, 11, 0 },
+                               { 0, 0, 0, 0, 0, 0, 0, 7, 4, 7, 0, 0, 7 },
+                               { 0, 0, 0, 0, 0, 0, 0, 8, 5, 11, 0, 0, 8 },
+                               { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 8, 0} };
       dijkstra(graph, 0);
     }
   }
