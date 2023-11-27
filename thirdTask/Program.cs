57de3cceac42845421f892ef1ec38e573667f677
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-11-27 13:05:22 +0400

    Added the method of Boyer and More algorithm

--- thirdTask/Program.cs
+++ thirdTask/Program.cs
@@ -71,9 +71,100 @@ namespace thirdTask
     }
 
     // method of Boyer and Moore algorithm
-    private static void s_boyerMooreAlgorithm()
+    private static void s_boyerMooreAlgorithm(char[] text, char[] pat)
     {
+      // s is shift of the pattern 
+      // with respect to text
+      int s = 0, j;
+      var m = pat.Length;
+      var n = text.Length;
 
+      var bpos = new int[m + 1];
+      var shift = new int[m + 1];
+
+      // initialize all occurrence of shift to 0
+      for (int i = 0; i < m + 1; i++)
+        shift[i] = 0;
+
+      // do preprocessing
+      s_preprocessStrongSuffix(shift, bpos, pat, m);
+      s_preprocessSecondCase(shift, bpos, pat, m);
+
+      while (s <= n - m)
+      {
+        j = m - 1;
+
+        /* Keep reducing index j of pattern while 
+        characters of pattern and text are matching 
+        at this shift s*/
+        while (j >= 0 && pat[j] == text[s + j])
+          j--;
+
+        /* If the pattern is present at the current shift, 
+        then index j will become -1 after the above loop */
+        if (j < 0)
+        {
+          Console.Write("Шаблонн содержится при сдвиге = {0}\n", s);
+          s += shift[0];
+        }
+        else
+          /*pat[i] != pat[s+j] so shift the pattern
+          shift[j+1] times */
+          s += shift[j + 1];
+      }
+    }
+        
+    /* Support methods */
+    //Preprocessing for case 2
+    private static void s_preprocessSecondCase(int[] shift, int[] bpos, char[] pat, int m)
+    {
+      int i, j;
+      j = bpos[0];
+      for (i = 0; i <= m; i++)
+      {
+        /* set the border position of the first character 
+        of the pattern to all indices in array shift
+        having shift[i] = 0 */
+        if (shift[i] == 0)
+          shift[i] = j;
+
+        /* suffix becomes shorter than bpos[0], 
+        use the position of next widest border
+        as value of j */
+        if (i == j)
+          j = bpos[j];
+      }
+    }
+
+    private static void s_preprocessStrongSuffix(int[] shift, int[] bpos, char[] pat, int m)
+    {
+      // m is the length of pattern 
+      int i = m, j = m + 1;
+      bpos[i] = j;
+
+      while (i > 0)
+      {
+        /*if character at position i-1 is not 
+        equivalent to character at j-1, then 
+        continue searching to right of the
+        pattern for border */
+        while (j <= m && pat[i - 1] != pat[j - 1])
+        {
+          /* the character preceding the occurrence of t 
+          in pattern P is different than the mismatching 
+          character in P, we stop skipping the occurrences 
+          and shift the pattern from i to j */
+            if (shift[j] == 0)
+              shift[j] = j - i;
+
+            //Update the position of next border 
+            j = bpos[j];
+        }
+        /* p[i-1] matched with p[j-1], border is found.
+        store the beginning position of border */
+        i--; j--;
+        bpos[i] = j;
+      }
     }
 
     /* Main method */
@@ -89,6 +180,12 @@ namespace thirdTask
       Console.WriteLine("Поиск алгоритмом Рабина-Карпа:");
       Console.WriteLine($"Текст для поиска: {text}");
       s_rubinKarpAlgorithm(pat, text, q);
+      Console.WriteLine();
+
+      Console.WriteLine("Поиск алгоритмом Бойера и Мура:");  
+      char[] text2 = "ABAAAABAACD".ToCharArray();
+      char[] pat2 = "ABA".ToCharArray();
+      s_boyerMooreAlgorithm(text2, pat2);
     }
   }
 }
