3551683f1798049572fec6a25f969511c4652893
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-11-27 13:42:09 +0400

    Created the seventh solution

---
+++ seventhTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace seventhTask
+{
+  internal class Program
+  {
+
+
+    /* Main method */
+    static void Main()
+    {
+      Console.WriteLine("Hello");
+    }
+  }
+}
