931a42c98b9006d219e6e787995f85c3229fa55d
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-11-28 12:30:26 +0400

    Created the fifth solution

---
+++ fifthTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace fifthTask
+{
+  internal class Program
+  {
+     /* Methods */
+
+
+     /* Main method */
+     private static void Main(string[] args)
+     {
+       Console.WriteLine("Hello");
+     }
+  }
+}
